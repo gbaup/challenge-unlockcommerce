ba059b7d311e7e7edb456372b48d2212ae9f760f
gastonbauer <gastonbauerp@gmail.com>
2025-01-22 10:33:55 -0300

    fin ejercicio 5

--- ejercicio5/carrito.js
+++ ejercicio5/carrito.js
@@ -1,8 +1,69 @@
-export function agregarProducto(carrito, producto) {
-    carrito.push(producto);
+function agregarProducto(carrito, producto, cantidad) {
+    if (typeof cantidad !== 'number' || cantidad < 1) {
+        return 'La cantidad debe ser mayor a 0';
+    }
+
+    if (producto.stockDisponible < cantidad) {
+        return 'No hay suficiente stock'
+    }
+
+    const productoEnCarrito = carrito.find(item => item.id === producto.id);
+
+    if (productoEnCarrito) {
+        productoEnCarrito.cantidad += cantidad;
+    } else {
+        carrito.push({...producto, cantidad});
+    }
+    return carrito;
 }
 
-export function verCarrito(carrito) {
+function verCarrito(carrito) {
     console.log(carrito);
 }
+
+function precioTotal(carrito, descuento) {
+    let total = 0;
+    for (let item of carrito) {
+        total += item.precio * item.cantidad;
+    }
+    if (typeof descuento === 'number' && descuento > 0) {
+        total = calcularPrecioFinal(total, descuento);
+    }
+    return total;
+
+}
+
+function calcularPrecioFinal(precio, descuento) {
+    if (typeof precio !== 'number' || precio < 0) {
+        return 'El precio debe ser mayor a 0';
+    }
+    if (typeof descuento !== 'number' || descuento < 0 || descuento > 100) {
+        return 'El descuento debe estar entre 0 y 100';
+    }
+
+    const precioFinal = precio - (precio * (descuento / 100));
+    return precioFinal.toFixed(2);
+}
+
+function simularPagoEnCuotas(monto, numeroCuotas, interesMensual) {
+    var cuota = monto / numeroCuotas;
+
+    var interesCompuesto = cuota * (1 + interesMensual / 100) ** numeroCuotas;
+
+    var cuotas = []
+
+    for (var i = 0; i < numeroCuotas; i++) {
+        cuotas.push({
+            numeroCuota: i + 1,
+            monto: interesCompuesto.toFixed(2)
+        });
+    }
+
+    return cuotas;
+}
+
+
+export {agregarProducto, verCarrito, precioTotal, simularPagoEnCuotas};
+
+
 
--- ejercicio5/sistema.js
+++ ejercicio5/sistema.js
@@ -1,10 +1,24 @@
-import {agregarProducto, verCarrito} from './carrito.js';
+import {agregarProducto, verCarrito, precioTotal, simularPagoEnCuotas} from './carrito.js';
+import productos from './productos.json' assert {type: 'json'};
 
-const carrito = [];
+const miCarrito = [];
 
+// Simulación de agregar un producto al carrito. Los productos van a ser obtenidos del json simulando información de una base de datos.
+const producto = productos[0];
+agregarProducto(miCarrito, producto, 2);
 
-verCarrito(carrito);
+// Simulación de agregar un producto sin stock al carrito
+const productoSinStock = productos[9];
+agregarProducto(miCarrito, productoSinStock, 5);
 
-agregarProducto(carrito, producto);
+const producto2 = productos[4];
+agregarProducto(miCarrito, producto2, 1);
 
-verCarrito(carrito);
+// Ver el carrito después de agregar el primer producto y no el segundo
+verCarrito(miCarrito);
+
+// Ver el precio total del carrito. La función precioTotal recibe un carrito y un descuento. Si no se pasa descuento, el precio total es el precio de los productos en el carrito.
+console.log(precioTotal(miCarrito, 10));
+
+// Simular el pago en cuotas
+console.log(simularPagoEnCuotas(precioTotal(miCarrito, 10), 6, 1));
